8ccae375072ce1fa813bd8975f448c52efddf2d7
Alec Dergachev <alecwithac27@gmail.com>
2022-10-27 11:47:20 -0400

    Tried making the buttons work
    (failed again)

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-window.onload = () => {
-    let rightButton = document.querySelector(`.button-R`);
-    let container = document.querySelector(`.big-container`);
-    rightButton.addEventListener(`click`, () => {
-        container.style.right = parseInt(container.style.right) - 465 + 'px';
-    });
-};
+//window.onload = () => {
+//    let rightButton = document.querySelector(`.button-R`);
+//    let container = document.querySelector(`.big-container`);
+//    rightButton.addEventListener(`click`, () => {
+//        container.style.right = parseInt(container.style.right) - 465 + 'px';
+//    });
+//};
+
+let container = document.querySelector(`.big-container`);
+
+window.addEventListener(`load`, () =>{
+    container.style.position = `absolute`;
+    container.style.right = 420;
+});
+
+window.addEventListener(`keyup`, (event) =>{
+  switch (event.key) {
+    case `ArrowRight`:
+        container.style.right = parseInt(container.style.right) - 50 + `px`;
+        break;
+  }
+});
