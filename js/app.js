b5d60d5ac7174c3fa1c345269469e7d426b71fa9
Alec Dergachev <alecwithac27@gmail.com>
2022-11-17 00:41:54 -0500

    Got my js to work

--- js/app.js
+++ js/app.js
@@ -1,22 +1,42 @@
-//window.onload = () => {
-//    let rightButton = document.querySelector(`.button-R`);
-//    let container = document.querySelector(`.big-container`);
-//    rightButton.addEventListener(`click`, () => {
-//        container.style.right = parseInt(container.style.right) - 465 + 'px';
-//    });
-//};
+window.onload = () => {
+  const leftButton = document.getElementById(`button-L`);
+  const rightButton = document.getElementById(`button-R`);
+  const parent = document.getElementsByClassName(`big-container`)[0];
+  const slide = document.getElementsByClassName(`container`)
+  let width = 0;
+  let index = 0;
 
-let container = document.querySelector(`.big-container`);
+  let checkSlide = () => {
+      if (index > 0 && index < 3) {
+          leftButton.classList.remove(`hidden`);
+          rightButton.classList.remove(`hidden`);
+      } else if (index === 0) {
+          leftButton.classList.add(`hidden`);
+      } else if (index === 3){
+          rightButton.classList.add(`hidden`);
+      }
+  };
 
-window.addEventListener(`load`, () =>{
-    container.style.position = `absolute`;
-    container.style.right = 420;
-});
+  rightButton.addEventListener(`click`, () => {
+      width -= 465;
+      parent.style.transform = `translate(`+width+`px)`;
+      index += 1;
+      checkSlide();
+  });
 
-window.addEventListener(`keyup`, (event) =>{
-  switch (event.key) {
-    case `ArrowRight`:
-        container.style.right = parseInt(container.style.right) - 50 + `px`;
-        break;
-  }
-});
+  leftButton.addEventListener(`click`, () => {
+      width += 465;
+      parent.style.transform = `translate(`+width+`px)`;
+      index -= 1;
+      checkSlide();
+  });
+
+  document.addEventListener(`keydown`, (e) => {
+      if (e.key === `ArrowRight` && !rightButton.classList.contains(`hidden`)) {
+          rightButton.click();
+      } else if (e.key === `ArrowLeft` && !leftButton.classList.contains(
+          `hidden`)) {
+          leftButton.click();
+      }
+  });
+};
